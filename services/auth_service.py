767cd1fb107454149abf34b921db6784e80353d9
Luong Thanh Tung <tung.luongthanh2004@hcmut.edu.vn>
2025-05-17 14:40:45 +0700

    Update JWT

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,20 +1,47 @@
 import jwt
 import datetime
 from flask import jsonify
-from config import Config
-from models.user import find_user_by_username
-from werkzeug.security import generate_password_hash, check_password_hash
+from config import SECRET_KEY
+from database.db import get_connection
+
 
 def login_user(data):
-    user = find_user_by_username(data['username'])
-    if user and check_password_hash(user['password'], data['password']):
-        token = jwt.encode({
-            'user_id': user['id'],
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)
-        }, Config.SECRET_KEY, algorithm='HS256')
-        return jsonify({'token': token})
-    return jsonify({'message': 'Invalid credentials'}), 401
+    username = data.get('username');
+    password = data.get('password');
+    conn = get_connection();
+    cursor = conn.cursor(dictionary=True)
+    query = "SELECT * FROM user WHERE username = %s"
+    cursor.execute(query, (username))
+    user = cursor.fetchone()
+    if user is None:
+        return jsonify({'message': 'Username not found'}), 404
+    elif user['password'] == password:
+        token = create_token(user['id']);
+        return jsonify({'token': token}), 200
+    else: 
+        return jsonify({'message': 'Invalid password'}), 401
 
 def register_user(data):
-    # Placeholder: Bạn sẽ thêm truy vấn insert thủ công sau
+    username = data.get('username');
+    password = data.get('password');
+    conn = get_connection();
+    cursor = conn.cursor(dictionary=True)
+    query = "SELECT * FROM user WHERE username = %s"
+    cursor.execute(query, (username))
+    user = cursor.fetchone()
+    if user is None:
+        return jsonify({'message': 'Username not found'}), 404
+    elif user['password'] == password:
+        token = create_token(user['id']);
+        return jsonify({'token': token}), 200
+    else: 
+        return jsonify({'message': 'Invalid password'}), 401
     return jsonify({'message': 'Register endpoint placeholder'})
+
+def create_token(user_id):
+    payload = {
+        'user_id': user_id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
