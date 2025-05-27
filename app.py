d54eb9ec1c9e78b610ab4e7f2b8edac57772e167
Luong Thanh Tung <tung.luongthanh2004@hcmut.edu.vn>
2025-05-28 00:01:38 +0700

    Final update

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 from routes.auth import auth_bp
 from routes.feeds import feeds_bp
 from routes.profile import profile_bp
 from services.mqtt_listener import start_mqtt
 import threading
-import eventlet
-from eventlet import wsgi
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Cho phép React kết nối
 
-#Tạo socket gửi dữ liệu tới client realtime
+# Gửi dữ liệu MQTT qua socket
 def handle_mqtt_message(feed_id, payload):
     socketio.emit('mqtt_message', {
         'feed': feed_id,
         'data': payload
     })
 
-# Bắt đầu MQTT trong thread riêng
+# Chạy MQTT ở thread riêng
 def start_background_mqtt():
-    start_mqtt(socketio, handle_mqtt_message)
     print("Starting MQTT client in background...")
+    start_mqtt(socketio, handle_mqtt_message)
 
-# Tạo một luồng riêng cho socket chạy vì không thể xử lý socket chạy chung với Flask
+# Start MQTT thread
 threading.Thread(target=start_background_mqtt).start()
 
+# Đăng ký blueprint
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(feeds_bp, url_prefix="/feeds")
 app.register_blueprint(profile_bp, url_prefix="/profile")
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
+    socketio.run(app, host='0.0.0.0', port=5000)  # ✅ dùng socketio.run thay vì eventlet.wsgi
